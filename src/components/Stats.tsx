4799c1a3ae48dd763c29421d9ee85454aa759fbb
Brent Grossman <baruchbenshlomo@gmail.com>
2025-12-01 11:03:03 -0800

    Add help tips

--- src/components/Stats.tsx
+++ src/components/Stats.tsx
@@ -9,12 +9,20 @@ export const Stats = () => {
 
   return (
     <div className="stats-panel">
-      <div className="stat-item">
-        <label>Xenogerm Efficiency:</label>
+      <div className="stat-item info"
+        title="Total metabolic efficiency contributed by xenogerm genes alone. You can't create the xenogerm if this is less than -5."
+      >
+        <span className="label">
+          Xenogerm Efficiency*:
+        </span>
         <span className={xenoWarning ? 'warning' : ''}>{totals.xenogermEfficiency}</span>
       </div>
-      <div className="stat-item">
-        <label>Final Efficiency:</label>
+      <div className="stat-item info"
+        title="Total metabolic efficiency after combining germline genes, suppressed genes, and xenogenes. You can't implant the xenogerm if this is less than -5."
+      >
+        <span className="label">
+          Final Efficiency*:
+        </span>
         <span className={finalWarning ? 'warning' : ''}>{totals.efficiency}</span>
       </div>
       <div className="stat-item">
@@ -22,8 +30,11 @@ export const Stats = () => {
         <span>{totals.complexity}</span>
       </div>
       <div className="status">
-        <span className={compatibleXenogerm ? 'good' : 'error'}>
-          {compatibleXenogerm ? 'Compatible' : 'Metabolic Efficiency too low'}
+        <span
+          className={compatibleXenogerm ? 'good' : 'error'}
+          title={compatibleXenogerm ? 'You can implant this xenogerm into the target germline.' : 'The metabolic efficiency is too low to implant this xenogerm.'}
+        >
+          {compatibleXenogerm ? 'Implantation Compatible' : 'Metabolic Efficiency too low to Implant'}
         </span>
       </div>
     </div>
